26d70e5cb4558f8b2237a5e407594144ba9d478f
Hejazapps <148637048+Hejazapps@users.noreply.github.com>
2026-09-12 19:42:34 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/main_tab_screen.dart';
 import 'screens/settings_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await SettingsThemeController.instance.load();
   runApp(const NovelAIApp());
 }
